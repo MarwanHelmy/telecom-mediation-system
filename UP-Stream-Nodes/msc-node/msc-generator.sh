6738b86ca5a22a38fdde208c99c22134ad0d715f
MarwanHelmy <marwan.helmy.elsheikh@gmail.com>
2026-05-07 18:37:39 +0300

    feat: updated MSC generator with zero-duration and massive-duration fraud scenarios

--- UP-Stream-Nodes/msc-node/msc-generator.sh
+++ UP-Stream-Nodes/msc-node/msc-generator.sh
@@ -5,20 +5,28 @@ while true; do
     TIMESTAMP=$(date +"%Y%m%d%H%M%S")
     FILENAME="MSC_CDR_${TIMESTAMP}.csv"
     
-    # Generate random duration between 0 and 299
-    DURATION=$((RANDOM % 300)) 
+    # Default normal values
+    DURATION=$((RANDOM % 300 + 1)) # Normal call: 1 to 300 seconds
+    CALLED_PARTY="2010$((RANDOM % 90000000 + 10000000))" # Normal number format
     
-    # Randomly generate normal numbers or short codes to test Java filtering
-    if [ $((RANDOM % 5)) -eq 0 ]; then
-        CALLED_PARTY="999"  # Short code
-    else
-        CALLED_PARTY="2010$((RANDOM % 90000000 + 10000000))" # Normal Egyptian number format
-    fi
+    # Randomly apply non-normal scenarios (10% chance for each scenario)
+    SCENARIO=$((RANDOM % 10))
     
-    # Randomly force a zero duration to test Java filtering
-    if [ $((RANDOM % 5)) -eq 0 ]; then
-        DURATION=0
-    fi
+    case $SCENARIO in
+        0)
+            # Scenario A: Short code
+            CALLED_PARTY="999"
+            ;;
+        1)
+            # Scenario B: Zero duration (dropped/free call)
+            DURATION=0
+            ;;
+        2)
+            # Scenario C: Massive duration (Fraud! 2 to 3 hours)
+            # 7200 seconds = 2 hours. This generates between 7200 and 10800 seconds.
+            DURATION=$((RANDOM % 3600 + 7200))
+            ;;
+    esac
     
     # Format: RecordType, CallingParty, CalledParty, Duration, Timestamp
     echo "1,201012345678,$CALLED_PARTY,$DURATION,$TIMESTAMP" > "$FTP_DIR/$FILENAME"
@@ -30,4 +38,3 @@ while true; do
     
     sleep 10
 done
-
