d99d81203d971f4483945ce57805f0d431068d5f
Mahmoud Salah <mahmoudalmasry0005@gmail.com>
2026-07-21 23:46:07 +0300

    refactor: Change static maven path dependency

--- Run_Mediation_System.sh
+++ Run_Mediation_System.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-
-export JAVA_HOME=$HOME/portable-java/current
-export PATH=$JAVA_HOME/bin:$PATH
-MAVEN=$HOME/portable-netbeans/current/java/maven/bin/mvn
-
 clear;
-cd $HOME/telecom-mediation-system/Mediation-System;
-$MAVEN clean compile > /dev/null 2>&1;
+cd Mediation-System;
+mvn clean compile > /dev/null 2>&1;
 
 clear;
 # run app (quiet mode)
-$MAVEN -q exec:java -Dexec.mainClass=Main_PK.MediationSystem;
+mvn -q exec:java;
 clear;
